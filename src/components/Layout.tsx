e4c42fe0835bed73466fdc090d75e6cc6c9fb115
scottvphan <114790711+scottvphan@users.noreply.github.com>
2023-12-05 13:10:34 -0500

    moved data fetch to root app file

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,30 +3,15 @@ import Navbar from "./Navbar";
 import { useEffect, useState } from "react";
 import Footer from "./Footer";
 
-export default function Layout() {
-  const [data, setData] = useState<any>();
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch("/data.json");
-      const jsonData = await response.json();
-      setData(jsonData);
-      console.log(data)
-    } catch (error) {
-      console.log(data)
-      console.error("Error fetching data:", error);
-    }
-  };
+export default function Layout(data:any) {
 
   return (
     <>
       <Navbar />
-      <Outlet />
+      <Outlet context={ data } />
       <Footer />
     </>
   );
 }
+
+// export const useLayoutContext = useOutletContext()
