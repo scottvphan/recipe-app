e4220b0c9872accc0087fc6599937a888a9ff374
scottvphan <114790711+scottvphan@users.noreply.github.com>
2023-12-05 11:07:45 -0500

    moved json to public folder and added data fetching

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,26 @@
 import { Outlet, useOutletContext } from "react-router-dom";
 import Navbar from "./Navbar";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Footer from "./Footer";
 
 export default function Layout() {
-  useEffect(() => {}, []);
+  const [data, setData] = useState<any>();
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("/data.json");
+      const jsonData = await response.json();
+      setData(jsonData);
+      console.log(data)
+    } catch (error) {
+      console.log(data)
+      console.error("Error fetching data:", error);
+    }
+  };
 
   return (
     <>
