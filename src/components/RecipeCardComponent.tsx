3bfa717547f0d3c15fa4d8dc62715709e0686117
scottvphan <114790711+scottvphan@users.noreply.github.com>
2023-12-06 16:41:44 -0500

    rendered different recipes based on the parameters of the link

--- src/components/RecipeCardComponent.tsx
+++ src/components/RecipeCardComponent.tsx
@@ -1,12 +1,14 @@
-
+import { NavLink } from "react-router-dom";
 export default function RecipeCardComponent({data}:any) {
-  
+  console.log(data)
   return (
-      <div className="bg-white p-6 rounded-lg shadow-md cursor-pointer">
-        <h3 className="text-xl font-semibold mb-2">{data.title}</h3>
-        <p className="text-gray-600">
-          {data.cuisine}
-        </p>
-      </div>
+    <NavLink to={`/recipe/${data.id}`}>
+        <div className="bg-white p-6 rounded-lg shadow-md cursor-pointer">
+          <h3 className="text-xl font-semibold mb-2">{data.title}</h3>
+          <p className="text-gray-600">
+            {data.cuisine}
+          </p>
+        </div>
+    </NavLink>
   );
 }
--- src/pages/RecipePage.tsx
+++ src/pages/RecipePage.tsx
@@ -1,16 +1,28 @@
 import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import RecipeIngredientsComponent from "../components/RecipeIngredientsComponent";
-import { useOutletContext } from "react-router-dom";
+import { useOutletContext, useParams, useNavigate } from "react-router-dom";
 
 export default function RecipePage() {
   const { data } = useOutletContext();
   const [ingredientCards, setIngredientCards] = useState<any>();
   const [instructionCards, setInstructionCards] = useState<any>();
-  const recipe = data[0];
+  const [recipe, setRecipe] = useState<any>("");
+  // const recipe = data[0]
+  const { id } = useParams();
+  const recipeId = Number(id);
+  const nav = useNavigate();
 
   useEffect(() => {
-    console.log(recipe);
+    const filteredRecipe = data.filter((recipes: any) => {
+      if (recipes.id === recipeId) {
+        return recipes;
+      }
+    });
+    setRecipe(filteredRecipe[0]);
+  }, []);
+
+  useEffect(() => {
     if (recipe) {
       const mappedIngredients = recipe.ingredients.map((ingredients: any) => {
         return <li key={uuidv4()}>{ingredients}</li>;
@@ -23,7 +35,8 @@ export default function RecipePage() {
       );
       setInstructionCards(mappedInstructions);
     }
-  }, []);
+  }, [recipe]);
+
   return (
     <div className="max-w-2xl mx-auto my-2 h-5/6 p-8 grid gap-4 ">
       <h1 className="text-3xl font-bold mb-2 ">{recipe.title}</h1>
@@ -39,16 +52,23 @@ export default function RecipePage() {
       </h1>
       <div>
         {ingredientCards && (
-          <ul className="list-disc pl-6 text-blue-500 grid gap-1">{ingredientCards}</ul>
+          <ul className="list-disc pl-6 text-blue-500 grid gap-1">
+            {ingredientCards}
+          </ul>
         )}
       </div>
       <div>
-        <h1 typeof="A" className="text-3xl font-bold border-blue-400 border-b-2 m-0 p-0">
+        <h1
+          typeof="A"
+          className="text-3xl font-bold border-blue-400 border-b-2 m-0 p-0"
+        >
           Instructions
         </h1>
         <div>
           {instructionCards && (
-            <ol className="list-decimal pl-6 text-blue-500 grid gap-1">{instructionCards}</ol>
+            <ol className="list-decimal pl-6 text-blue-500 grid gap-1">
+              {instructionCards}
+            </ol>
           )}
         </div>
       </div>
